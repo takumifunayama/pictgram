d92264d1c04f3c792b58017c2dc29c1404c22bc4
takumifunayama <mt.koala01n@gmail.com>
2019-09-22 15:50:32 +0000

    2019年

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length:{maximum: 15}
+    validates :email, presence: true
+    validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+    validates :password, length:{in:8..32}
+    validates :password, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+    
+    has_secure_password
+end
