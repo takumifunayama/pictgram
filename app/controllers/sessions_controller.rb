0effd475f4b1b9e84aa2cc41e8c98c7d5099e010
takumifunayama <mt.koala01n@gmail.com>
2019-09-24 13:02:17 +0000

    Dry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,23 @@ class SessionsController < ApplicationController
     end
   end
   
+  def destroy
+    log_out
+    redirect_to root_url, info:'ログアウトしました'
+  end
+  
   private
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  def log_out
+    session.delete(:user_id)
+    @current_user = nill
+  end
 end
+
+private
+  def user_params
+    params.require(:user).permit(:name,:email,:password, :password_confirmation)
+  end
