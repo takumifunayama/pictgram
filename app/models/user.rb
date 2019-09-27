7261e6969b30ad9c2fa6cd2f5f6e0f5da0449496
takumifunayama <mt.koala01n@gmail.com>
2019-09-27 15:33:53 +0000

    login修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     validates :email, presence: true
     validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
     validates :password, length:{in:8..32}
-    validates :password, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+    #validates :password, format:{with:/\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
     
     has_secure_password
 end
