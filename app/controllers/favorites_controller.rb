ffcb0fa22332b548e30f61c0e345f0bcd60dcfbd
takumifunayama <mt.koala01n@gmail.com>
2019-10-30 11:13:17 +0000

    いいね実装暫定

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,5 +13,10 @@ class FavoritesController < ApplicationController
   else
     redirect_to topics_path,danger: 'お気に入りの登録に失敗しました'
   end
+
+  def destroy
+    @favorite = Favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to topics_path, success: 'お気に入りを取り消しました'
   end
 end
